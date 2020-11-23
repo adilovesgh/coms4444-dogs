47b2a02618d8ae0109f400f1d6d1ab6385c104e3
jeffreyskline <jeffreyskline@outlook.com>
2020-11-23 10:38:56 -0500

    solo-minded player, only exercises his own dogs once he arrives at random location some distance away from others.

--- src/dogs/g5/Player.java
+++ src/dogs/g5/Player.java
@@ -74,21 +74,23 @@ public class Player extends dogs.sim.Player {
 			return directive;
 		}
 
+		
+
 		//get to target location //////todo 2
 		////calc slope, go towards it
 		if(myOwner.getLocation().getColumn() < targetColumn || myOwner.getLocation().getRow() < targetRow) {
-            double rowDelta = myOwner.getLocation().getRow() - targetRow;
-            double colDelta = myOwner.getLocation().getColumn() - targetColumn;
+			simPrinter.println(myOwner.getLocation().toString());
+			double rowDelta = myOwner.getLocation().getRow() - targetRow;
+			double colDelta = myOwner.getLocation().getColumn() - targetColumn;
 
-            double angle = Math.atan(rowDelta/colDelta);
+			double angle = Math.atan(rowDelta/colDelta);
 
-            double scaledRow = 4.99*Math.sin(angle);
-            double scaledCol = 4.99*Math.cos(angle);
+			double scaledRow = 4.99*Math.sin(angle);
+			double scaledCol = 4.99*Math.cos(angle);
 
-            directive.instruction = Instruction.MOVE;
-    		directive.parkLocation = new ParkLocation(myOwner.getLocation().getRow() + scaledRow, myOwner.getLocation().getColumn() + scaledCol);
-    		return directive;
-
+			directive.instruction = Instruction.MOVE;
+			directive.parkLocation = new ParkLocation(myOwner.getLocation().getRow() + scaledRow, myOwner.getLocation().getColumn() + scaledCol);
+			return directive;
 		}
 
 		//pt4:
@@ -98,42 +100,19 @@ public class Player extends dogs.sim.Player {
 		/////pt 1sort dogs, ignore dogs that aren't ours
 		//pt 2: direction that we through them
 		//pt 3: distance (hardcoded 9m)
-		
 
+		List<Dog> waitingDogs = getWaitingDogs(myOwner, otherOwners);
 
+		if(waitingDogs.size() > 0 && ){ 
+			directive.instruction = Instruction.THROW_BALL;
+			simPrinter.println(waitingDogs);
+			directive.dogToPlayWith = waitingDogs.get(random.nextInt(waitingDogs.size()));
 
+			double randomAngle = Math.toRadians(random.nextDouble() * 360);
+			double ballRow = myOwner.getLocation().getRow() + 40.0 * Math.sin(randomAngle);
+			double ballColumn = myOwner.getLocation().getColumn() + 40.0 * Math.cos(randomAngle);
+			simPrinter.println("THROW ROW: " + ballRow +  " THROW COLUMN: " + ballColumn);
 
-
-		
-    	
-    	/*if(round <= 151) {
-    		directive.instruction = Instruction.MOVE;
-    		directive.parkLocation = new ParkLocation(myOwner.getLocation().getRow() + 6, myOwner.getLocation().getColumn() + 6);
-    		return directive;
-    	}
-    	
-    	List<Instruction> instructions = new ArrayList<>(Arrays.asList(Instruction.values()));
-    	instructions.remove(Instruction.EXIT_PARK);
-
-    	int instructionIndex = random.nextInt(instructions.size());
-    	Instruction chosenInstruction = instructions.get(instructionIndex);
-    	directive.instruction = chosenInstruction;
-    	    	
-    	double randomDistance, randomAngle;    	
-    	switch(chosenInstruction) {
-    	case THROW_BALL:
-			List<Dog> waitingDogs = getWaitingDogs(myOwner, otherOwners);
-			if(waitingDogs.size() > 0)
-				directive.dogToPlayWith = waitingDogs.get(random.nextInt(waitingDogs.size()));
-			else {
-				directive.instruction = Instruction.NOTHING;
-				break;
-			}
-    		
-			randomDistance = random.nextDouble() * 40.0;
-			randomAngle = Math.toRadians(random.nextDouble() * 360);
-			double ballRow = myOwner.getLocation().getRow() + randomDistance * Math.sin(randomAngle);
-			double ballColumn = myOwner.getLocation().getColumn() + randomDistance * Math.cos(randomAngle);
 			if(ballRow < 0.0)
 				ballRow = 0.0;
 			if(ballRow > ParkLocation.PARK_SIZE - 1)
@@ -142,39 +121,11 @@ public class Player extends dogs.sim.Player {
 				ballColumn = 0.0;
 			if(ballColumn > ParkLocation.PARK_SIZE - 1)
 				ballColumn = ParkLocation.PARK_SIZE - 1;
+			
 			directive.parkLocation = new ParkLocation(ballRow, ballColumn);
-
-			break;
-    	case MOVE:
-			randomDistance = random.nextDouble() * 5.0;
-			randomAngle = Math.toRadians(random.nextDouble() * 360);
-			double newOwnerRow = myOwner.getLocation().getRow() + randomDistance * Math.sin(randomAngle);
-			double newOwnerColumn = myOwner.getLocation().getColumn() + randomDistance * Math.cos(randomAngle);
-			if(newOwnerRow < 0.0)
-				newOwnerRow = 0.0;
-			if(newOwnerRow > ParkLocation.PARK_SIZE - 1)
-				newOwnerRow = ParkLocation.PARK_SIZE - 1;
-			if(newOwnerColumn < 0.0)
-				newOwnerColumn = 0.0;
-			if(newOwnerColumn > ParkLocation.PARK_SIZE - 1)
-				newOwnerColumn = ParkLocation.PARK_SIZE - 1;
-			directive.parkLocation = new ParkLocation(newOwnerRow, newOwnerColumn);			
-    		break;
-    	case CALL_SIGNAL:
-    		List<String> otherOwnersPrevRoundWords = getOtherOwnersSignals(otherOwners);
-    		
-    		List<String> words = Dictionary.words;
-        	words.remove("_");
-        	if(random.nextInt(10) < 10 * listeningProbability && !otherOwnersPrevRoundWords.isEmpty())
-        		directive.signalWord = otherOwnersPrevRoundWords.get(random.nextInt(otherOwnersPrevRoundWords.size()));
-        	else
-        		directive.signalWord = words.get(random.nextInt(words.size()));
-    		break;
-    	case NOTHING:
-    		break;
-		default:
-			break;
-    	}*/
+		} else {
+				
+		}
     	    	
 		return directive;
     }
@@ -218,10 +169,18 @@ public class Player extends dogs.sim.Player {
         coordinates.add(rowVal);
         coordinates.add(colVal);
 
-        System.out.println("row is " + rowVal);
-        System.out.println("col is " + colVal);
+        simPrinter.println("row is " + rowVal);
+        simPrinter.println("col is " + colVal);
 
         return coordinates;
+    }
+
+    private List<String> pickNeedyDog(List<Owner> otherOwners) {
+    	List<String> otherOwnersSignals = new ArrayList<>();
+    	for(Owner otherOwner : otherOwners)
+    		if(!otherOwner.getCurrentSignal().equals("_"))
+    			otherOwnersSignals.add(otherOwner.getCurrentSignal());
+    	return otherOwnersSignals;
     }
     
     private List<String> getOtherOwnersSignals(List<Owner> otherOwners) {
@@ -238,12 +197,13 @@ public class Player extends dogs.sim.Player {
     		if(dog.isWaitingForItsOwner())
     			waitingDogs.add(dog);
     	}
+    	/*
     	for(Owner otherOwner : otherOwners) {
     		for(Dog dog : otherOwner.getDogs()) {
     			if(dog.isWaitingForOwner(myOwner))
     				waitingDogs.add(dog);
     		}
-    	}
+    	}*/
     	return waitingDogs;
     }
 }
