a3fb8e66959b5585ea4675c8f494e776fe59ada1
Nuneke <nfk2108@columbia.edu>
2020-11-30 12:27:34 -0500

    Improvement on deliverable 1.

--- src/dogs/g2/Player.java
+++ src/dogs/g2/Player.java
@@ -46,26 +46,38 @@ public class Player extends dogs.sim.Player {
             directive.signalWord = "aardwolf";
             return directive;
         } 
-        List<ParkLocation> initialLocations = equidistantPtsOnCircle(otherOwners);
-        ParkLocation myLocation = initialLocations.get(1);
-        while (myOwner.getLocation().getRow() <= myLocation.getRow() || myOwner.getLocation().getColumn() <= myLocation.getColumn()) {
+        ParkLocation myLocation = equidistantPtsOnCircle(myOwner, otherOwners);
+        //ParkLocation myLocation = initialLocations.get(1);
+        while (myOwner.getLocation().getRow() < myLocation.getRow() || myOwner.getLocation().getColumn() < myLocation.getColumn()) {
             directive.instruction = Instruction.MOVE;
-            directive.parkLocation = new ParkLocation(myOwner.getLocation().getRow() + 2, myOwner.getLocation().getColumn() + 2);
+            double rowDiff = Math.max(0,Math.min(myLocation.getRow() - myOwner.getLocation().getRow(), 3.5));
+            double colDiff = Math.max(0,Math.min(myLocation.getColumn() - myOwner.getLocation().getColumn(), 3.5));
+
+            directive.parkLocation = new ParkLocation(myOwner.getLocation().getRow() + rowDiff, myOwner.getLocation().getColumn() + colDiff);
             return directive;
         }
 
+        //System.out.println(myOwner.getNameAsEnum().ordinal() + " " + myOwner.getLocation().getRow() + " " + myOwner.getLocation().getColumn());
 
-        // Sort dogs by speed 
-        List<Dog> waitingDogs = getWaitingDogs(myOwner, otherOwners);
+        if(round <= 70) {
+            directive.instruction = Instruction.CALL_SIGNAL;
+            directive.signalWord = "aardwolf";
+            return directive;
+        }
+
+        List<Dog> waitingDogs = getAllWaitingDogs(myOwner, otherOwners);
+        //debug(myOwner, otherOwners);
 
         Comparator<Dog> speed = (Dog dog1, Dog dog2) -> Double.compare(dog1.getRunningSpeed(), dog2.getRunningSpeed());
         Collections.sort(waitingDogs, speed);
 
+        //System.out.println("size: " + waitingDogs.size());
 
         //Throw Ball in the direction of other owners
         if(waitingDogs.size() > 0){
             directive.instruction = Instruction.THROW_BALL;
             directive.dogToPlayWith = waitingDogs.get(0);
+            directive.parkLocation = getBallDirection(myOwner, otherOwners);
 
             //directive.parkLocation = otherOwners.get(randomNum).getLocation();
         } else {
@@ -73,7 +85,7 @@ public class Player extends dogs.sim.Player {
             directive.instruction = Instruction.NOTHING;
         }
 
-
+/*
         double randomDistance, randomAngle;             
         randomDistance = 40.0;
         randomAngle = Math.toRadians(random.nextDouble() * 360);
@@ -91,22 +103,39 @@ public class Player extends dogs.sim.Player {
         if(ballColumn > ParkLocation.PARK_SIZE - 1)
             ballColumn = ParkLocation.PARK_SIZE - 1;
         directive.parkLocation = new ParkLocation(ballRow, ballColumn);
+        */
         return directive;
     }
 
-    private List<ParkLocation> equidistantPtsOnCircle(List<Owner> otherOwners){
+    private ParkLocation equidistantPtsOnCircle(Owner myOwner, List<Owner> otherOwners){
         //Size of the MAP is 200 x 200
-        List<ParkLocation> initialParkLocations = new ArrayList<>();
-        int radius = 10;
-        int theta = 360/(otherOwners.size()+1);
-        for ( int i = 1; i <= otherOwners.size()+1; i++ ) {
-            Double ownerX = ( radius * Math.cos(theta * i) + 100 );
-            Double ownerY = ( radius * Math.sin(theta * i) + 100 );
-            ParkLocation pkloc = new ParkLocation(ownerX, ownerY);
-            initialParkLocations.add(pkloc);
-        }
-        return initialParkLocations;
+        double angle = (2 * Math.PI) / (otherOwners.size() + 1);
+
+        double radius = 40 / (2 * Math.sin(2 * Math.PI / (otherOwners.size() + 1)));
+
+        double row = Math.round(radius - Math.round(radius) * Math.sin(myOwner.getNameAsEnum().ordinal() * angle));
+        double column = Math.round(radius - Math.round(radius) * Math.cos(myOwner.getNameAsEnum().ordinal()* angle));
+
+        //System.out.println(myOwner.getNameAsEnum().ordinal() + " " + row + " " + column);
+        ParkLocation initialParkLocation = new ParkLocation(row, column);
+        return initialParkLocation;
     }
+
+    private ParkLocation getBallDirection(Owner myOwner, List<Owner> otherOwners){
+        //Size of the MAP is 200 x 200
+
+        double angle = (2 * Math.PI) / (otherOwners.size() + 1);
+
+        double radius = 39 / (2 * Math.sin(2 * Math.PI / (otherOwners.size() + 1)));
+
+        double row = Math.round(radius - Math.round(radius) * Math.sin(((myOwner.getNameAsEnum().ordinal() + 1 ) % (otherOwners.size() + 1)) * angle));
+        double column = Math.round(radius - Math.round(radius) * Math.cos(((myOwner.getNameAsEnum().ordinal() + 1 ) % (otherOwners.size() + 1)) * angle));
+
+        //System.out.println(((myOwner.getNameAsEnum().ordinal() + 1 ) % (otherOwners.size() + 1)));
+        ParkLocation initialParkLocation = new ParkLocation(row, column);
+        return initialParkLocation;
+    }
+
 
     private List<String> getOtherOwnersSignals(List<Owner> otherOwners) {
         List<String> otherOwnersSignals = new ArrayList<>();
@@ -116,19 +145,85 @@ public class Player extends dogs.sim.Player {
         return otherOwnersSignals;
     }
 
-    private List<Dog> getWaitingDogs(Owner myOwner, List<Owner> otherOwners) {
+    private List<Dog> getMyWaitingDogs(Owner myOwner, List<Owner> otherOwners) {
         List<Dog> waitingDogs = new ArrayList<>();
         for(Dog dog : myOwner.getDogs()) {
-            if(dog.isWaitingForItsOwner())
+            if(dog.isWaitingForItsOwner() && dog.getExerciseTimeRemaining() > 0.0)
                 waitingDogs.add(dog);
         }
+        return waitingDogs;
+    }
+
+    private List<Dog> getOtherWaitingDogs(Owner myOwner, List<Owner> otherOwners) {
+        List<Dog> waitingDogs = new ArrayList<>();
+    
         for(Owner otherOwner : otherOwners) {
             for(Dog dog : otherOwner.getDogs()) {
-                if(dog.isWaitingForOwner(myOwner))
+                if(dog.isWaitingForOwner(myOwner) && dog.getExerciseTimeRemaining() > 0.0)
                     waitingDogs.add(dog);
             }
         }
         return waitingDogs;
+    }
+
+    private List<Dog> getAllWaitingDogs(Owner myOwner, List<Owner> otherOwners) {
+        List<Dog> waitingDogs = new ArrayList<>();
+        for(Dog dog : myOwner.getDogs()) {
+            if(dog.isWaitingForItsOwner())
+                //if(dog.getExerciseTimeRemaining() > 0.0) {
+                    waitingDogs.add(dog);
+                //}
+        }
+        for(Owner otherOwner : otherOwners) {
+            for(Dog dog : otherOwner.getDogs()) {
+                if(!dog.isMoving()){
+                    if (dog.getOwnerWaitingFor().getNameAsEnum().ordinal() == myOwner.getNameAsEnum().ordinal()){
+                        //if(dog.getExerciseTimeRemaining() > 0.0) {
+                            //System.out.println(dog.getBreed());
+                            waitingDogs.add(dog);
+                        //}
+                    }    
+                }     
+            }
+        }
+        //System.out.println(waitingDogs.size());
+        return waitingDogs;
+    }
+
+    private Dog getDog(List<Dog> waitingDogs) {
+        int count = 0;
+        for (Dog dog : waitingDogs) {
+            //System.out.println(dog.getBreed());
+            //System.out.println(dog.getExerciseTimeRemaining());
+            if (dog.getExerciseTimeRemaining() != 0.0) {
+                System.out.println(count);
+                return dog;
+            }
+            count ++;
+        }
+
+        return null;
+    }
+
+    private void debug(Owner myOwner, List<Owner> otherOwners) {
+
+        for(Dog dog : myOwner.getDogs()) {
+            System.out.println(dog.getBreed());
+            System.out.println(dog.getLocation());
+            System.out.println(dog.getOwner().getNameAsEnum().ordinal());
+            System.out.println(dog.isMoving());
+            System.out.println(dog.getOwnerWaitingFor().getNameAsEnum().ordinal());
+        }
+        for(Owner otherOwner : otherOwners) {
+            for(Dog dog : otherOwner.getDogs()) {
+                System.out.println(dog.getBreed());
+                System.out.println(dog.getLocation());
+                System.out.println(dog.getOwner().getNameAsEnum().ordinal());
+                System.out.println(dog.isMoving());
+                System.out.println(dog.getOwnerWaitingFor().getNameAsEnum().ordinal());
+            }
+        }
+
     }
     
 }
